9d87c2fdb6b6c13b5b8ca054816445fddc4cdbbe
aygrhn1991 <411610370@qq.com>
2020-05-27 15:03:12 +0800

    1

--- src/main/java/com/htkj/xcx/ApiCtrl.java
+++ src/main/java/com/htkj/xcx/ApiCtrl.java
@@ -1,9 +1,11 @@
 package com.htkj.xcx;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -19,7 +21,17 @@ public class ApiCtrl {
     @ResponseBody
     public Map code(@PathVariable String code) {
         String url = String.format("https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code", this.appid, this.appsecret, code);
-        Map map = new RestTemplate().getForObject(url, Map.class);
+        String result = new RestTemplate().getForObject(url, String.class);
+        Map map = new Gson().fromJson(result, Map.class);
+        return map;
+    }
+
+    @RequestMapping("/addJob/{openid}")
+    @ResponseBody
+    public Map addJob(@PathVariable String openid) {
+        Map map = new HashMap();
+        map.put("result", "ok");
+        map.put("openid", openid);
         return map;
     }
 }
